5a35ec37ca098bd11e656d6b2036a197e1a88d10
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2022-01-26 12:55:59 +0200

    commit to main

---
+++ src/components/filter/Filter.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Label = styled.label`
+ margin-top:10px;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 1.25;
+
+`;
+
+export const Input = styled.input`
+ padding-left: 10px;
+`;
